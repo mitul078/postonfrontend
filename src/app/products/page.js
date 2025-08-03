8b7a43828a91c59c45d8557ce363191f9a0e3d64
mitul078 <mituljodhani788@gmail.com>
2025-08-03 18:12:41 +0530

    productShowCase Good

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -6,10 +6,12 @@ import { useState, useEffect } from 'react'
 import Spinner from '@/components/Spinner'
 import toast from 'react-hot-toast'
 
+
 const ProductPage = () => {
     const [loading, setLoading] = useState(false)
     const [products, setProducts] = useState([])
     const [addToCartLoading, setAddToCartLoading] = useState({});
+    const [selected, setSelected] = useState({})
     useEffect(() => {
         const getProducts = async () => {
             setLoading(true)
@@ -35,21 +37,37 @@ const ProductPage = () => {
             toast.success("Added to cart")
         } catch (error) {
             console.log(error)
-        }finally{
+        } finally {
             setAddToCartLoading(prev => ({ ...prev, [pid]: false }));
         }
     }
 
     return (
         <div className='ProductPage' >
+            <div className="navbar">
+
+            </div>
             <div className="container">
                 {loading ? <Spinner /> : (
                     products.map((product, i) => (
                         <div key={i} className="box">
                             <div className="name">{product.name}</div>
-                            <div className="image"><img src={product.images[0]} alt="" /></div>
+                            <div className="image">
+                                <img src={selected[product._id] || product.images[0]} alt="" />
+
+
+                                <div className="b">
+                                    {
+                                        product.images.slice(1, 4).map((img, i) => (
+                                            <div onClick={() => setSelected(prev => ({ ...prev, [product._id]: img }))} key={i} className="b1">
+                                                <img src={img} alt="" />
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
                             <div className="des">{product.description}</div>
-                            <div className="price">{product.price}</div>
+                            <div className="price">$ <span>{product.price}</span></div>
                             <div className="btn">
                                 <button>See Details</button>
                                 <button disabled={addToCartLoading[product._id]} onClick={() => addToCart(product._id)}>{addToCartLoading[product._id] ? "Adding..." : "Add to Cart"}</button>
